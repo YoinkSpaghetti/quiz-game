743f558ba2ee69afa8a42e159e8722bae623f1c0
YoinkSpaghetti <michael.mo23@sitechhs.com>
2021-04-08 17:59:16 -0400

    asd

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,8 @@
-/* document.getElementById("yesBtn").addEventListener(
+document.getElementById("yesBtn").addEventListener(
   "click",
   function () {
     document.getElementById("startQuiz").hidden = true;
     document.getElementById("questions").hidden = false;
   },
   false
-); */
-function onoff() {
-  if (document.getElementById)
-    document.getElementById("yesBtn").style.visibility = "hidden";
-}
+);
