acd348482efd334bdd67525b9aca9a7f9e0ed9b1
YoinkSpaghetti <michael.mo23@sitechhs.com>
2021-04-08 21:49:11 -0400

    why does everything have to be so complicated

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,24 @@
-document.getElementById("yesBtn").addEventListener(
-  "click",
-  function () {
-    document.getElementById("startQuiz").hidden = true;
-    document.getElementById("questions").hidden = false;
-  },
-  false
-);
+x = 0;
+y = true;
+z = false;
+while (x < 7) {
+  if (x == 0) {
+    y = true;
+  } else {
+    y = false;
+  }
+  if (x == 1) {
+    z = true;
+  } else {
+    z = false;
+  }
+  document.getElementById("yesBtn").addEventListener(
+    "click",
+    function () {
+      document.getElementById("startQuiz").hidden = y;
+      document.getElementById("questions").hidden = z;
+    },
+    (x = x + 1),
+    false
+  );
+}
