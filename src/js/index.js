4041b6901ba96674c8f8ef02f4a65c1fa209b160
YoinkSpaghetti <71717533+YoinkSpaghetti@users.noreply.github.com>
2021-04-09 10:00:40 -0400

    spaghetti

--- src/js/index.js
+++ src/js/index.js
@@ -1,24 +1,9 @@
-x = 0;
-y = true;
-z = false;
-while (x < 7) {
-  if (x == 0) {
-    y = true;
-  } else {
-    y = false;
-  }
-  if (x == 1) {
-    z = true;
-  } else {
-    z = false;
-  }
-  document.getElementById("yesBtn").addEventListener(
-    "click",
-    function () {
-      document.getElementById("startQuiz").hidden = y;
-      document.getElementById("questions").hidden = z;
-    },
-    (x = x + 1),
-    false
-  );
-}
+
+    function myFunction() {
+        var x = document.getElementById("startQuiz");
+        if (x.style.display === "none") {
+          x.style.display = "block";
+        } else {
+          x.style.display = "none";
+        }
+      }
