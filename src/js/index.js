8c5550bd0691b318c66400a8b99dfbcbf3a77a68
YoinkSpaghetti <michael.mo23@sitechhs.com>
2021-04-13 13:51:51 -0400

    finished but severely broken

--- src/js/index.js
+++ src/js/index.js
@@ -9,18 +9,20 @@ var btnThreeOne = document.getElementById("firstAnswer");
 var btnThreeTwo = document.getElementById("secondAnswer");
 var btnThreeThree = document.getElementById("thirdAnswer");
 var btnThreeFour = document.getElementById("fourthAnswer");
+var btnFourOne = document.getElementById("continueButtonOne");
+var btnFourTwo = document.getElementById("continueButtonTwo");
 let score = 0;
 /* var timerLength = document.getElementById("timerid");
 var a = timerLength.style.width; */
-var questionNumb = 0;
+questionNumb = 0;
 
-var rightAnswer = ["4", "Orange", "spaghetti", "no"];
+var rightAnswer = ["4", "Orange", "a stick", "I don't know"];
 var wrongAnswer = [
   "click me",
-  "I dont know",
+  "no",
   "yellow",
   "Justin Bieber",
-  "Adam Sandlers",
+  "Chuck Norris",
   "Pringles",
   "9999",
   "yes",
@@ -29,6 +31,7 @@ var wrongAnswer = [
   "hello",
   "Durito Bandito",
 ];
+
 //switches page
 function functionOne() {
   x.style.display = "none";
@@ -55,11 +58,6 @@ function timerFunction() {
   }
 } */
 
-function makeAnswers() {
-  var text = document.createTextNode();
-  var element = document.getElementById("new");
-  element.appendChild(text);
-}
 //switches page
 function functionThreeRight() {
   z.style.display = "none";
@@ -71,54 +69,23 @@ function functionThreeRight() {
 function functionThreeWrong() {
   z.style.display = "none";
   oneWrong.style.display = "block";
-  console.log(score);
 }
 
-function randomNumber(min, max) {
-  return Math.floor(Math.random() * (max - min) + min);
-}
-//dunno how to shuffle arrays so I made it a butte-tonn lot more harder for myself
-function questionMaker(n, nOne, nTwo, nThree, nFour, nFive) {
-  //taking a random value from wrong Answer and the question number of right Answer and putting them in an array
-  var n = randomNumber(0, 12);
-  var nOne = randomNumber(0, 11);
-  var nTwo = randomNumber(0, 10);
-  var nThree = randomNumber(0, 4);
-  var nFour = randomNumber(0, 3);
-  var nFive = randomNumber(0, 2);
-  allAnswers = [rightAnswer[questionNumb]];
-  var randomNumbOne = n;
-  allAnswers.push(wrongAnswer[randomNumbOne]);
-  wrongAnswer.splice(randomNumbOne, 1);
-  console.log(wrongAnswer);
-  var randomNumbTwo = nOne;
-  allAnswers.push(wrongAnswer[randomNumbTwo]);
-  wrongAnswer.splice(randomNumbTwo, 1);
-  var randomNumbThree = nTwo;
-  allAnswers.push(wrongAnswer[randomNumbThree]);
-  wrongAnswer.splice(randomNumbThree, 1);
-  console.log(allAnswers);
-  //then putting it a new shuffled array
-  var allAnswersTwo = [];
-  var randomNumbFour = nThree;
-  allAnswersTwo.push(allAnswers[randomNumbFour]);
-  allAnswers.splice(randomNumbFour, 1);
-  console.log(allAnswers);
-  var randomNumbFive = nFour;
-  allAnswersTwo.push(allAnswers[randomNumbFive]);
-  allAnswers.splice(randomNumbFive, 1);
-  var randomNumbSix = nFive;
-  allAnswersTwo.push(allAnswers[randomNumbSix]);
-  allAnswers.splice(randomNumbSix, 1);
-  allAnswersTwo.push(allAnswers[0]);
-  document.getElementById("firstAnswer").innerHTML = allAnswersTwo[0];
-  document.getElementById("secondAnswer").innerHTML = allAnswersTwo[1];
-  document.getElementById("thirdAnswer").innerHTML = allAnswersTwo[2];
-  document.getElementById("fourthAnswer").innerHTML = allAnswersTwo[3];
-  console.log(allAnswersTwo);
+function functionFourRight() {
+  oneCorrect.style.display = "none";
+  z.style.display = "block";
+  questionNumb = questionNumb + 1;
+  console.log(questionNumb);
 }
 
-function questionNumba(sheep) {
+function functionFourWrong() {
+  oneWrong.style.display = "none";
+  z.style.display = "block";
+  questionNumb = questionNumb + 1;
+  console.log(questionNumb);
+}
+
+function questionNumbar(sheep) {
   if (sheep == 0) {
     document.getElementById("questionId").innerHTML = "what is 2 + 2?";
   }
@@ -127,14 +94,56 @@ function questionNumba(sheep) {
   }
   if (sheep == 2) {
     document.getElementById("questionId").innerHTML =
-      "I hope she made lotsa ___";
+      "What is brown and sticky";
   }
   if (sheep == 3) {
     document.getElementById("questionId").innerHTML =
       "what color underwear am I wearing right now?";
   }
+  if (sheep > 3) {
+    document.getElementById("questionId").innerHTML =
+      "thanks for playing my rigged quiz";
+  }
 }
-questionNumba(questionNumb);
+
+function randomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
+}
+//dunno how to shuffle arrays so I made it a butte-tonn lot more harder for myself
+function questionMaker() {
+  //taking a random value from wrong Answer and the question number of right Answer and putting them in an array
+  questionNumbar(questionNumb);
+  var n = randomNumber(0, wrongAnswer.length);
+  var nOne = randomNumber(0, wrongAnswer.length - 1);
+  var nTwo = randomNumber(0, wrongAnswer.length - 2);
+  var nThree = randomNumber(0, 4);
+  var nFour = randomNumber(0, 3);
+  var nFive = randomNumber(0, 2);
+  var nSix = randomNumber(0, 1);
+  allAnswers = [rightAnswer[questionNumb]];
+  function insideQuestionMaker(banana) {
+    allAnswers.push(wrongAnswer[banana]);
+    wrongAnswer.splice(banana, 1);
+  }
+  insideQuestionMaker(n); //three wrong answers
+  insideQuestionMaker(nOne);
+  insideQuestionMaker(nTwo);
+  //then putting it a new shuffled array
+  var allAnswersTwo = [];
+  function insideQuestionMakerAgain(apple) {
+    allAnswersTwo.push(allAnswers[apple]);
+    allAnswers.splice(apple, 1);
+  }
+  insideQuestionMakerAgain(nThree); //four shuffled answers
+  insideQuestionMakerAgain(nFour);
+  insideQuestionMakerAgain(nFive);
+  insideQuestionMakerAgain(nSix);
+  document.getElementById("firstAnswer").innerHTML = allAnswersTwo[0];
+  document.getElementById("secondAnswer").innerHTML = allAnswersTwo[1];
+  document.getElementById("thirdAnswer").innerHTML = allAnswersTwo[2];
+  document.getElementById("fourthAnswer").innerHTML = allAnswersTwo[3];
+  console.log(allAnswersTwo);
+}
 
 btn.addEventListener("click", functionOne);
 btnTwo.addEventListener("click", functionTwo);
@@ -146,8 +155,8 @@ btnTwo.addEventListener("click", functionTwo);
 
 btnTwo.addEventListener("click", questionMaker);
 
-//insert an if questionNumb === 1 or something
-//checks if answer chosen is correct or wrong and showing the right page
+//insert an if questionNumb === 0 or something
+//checks if answer chosen is correct or wrong and showing the right page which doesn't work
 if (document.getElementById("firstAnswer").innerHTML === "4") {
   btnThreeOne.addEventListener("click", functionThreeRight);
 } else {
@@ -168,3 +177,10 @@ if (document.getElementById("fourthAnswer").innerHTML === "4") {
 } else {
   btnThreeFour.addEventListener("click", functionThreeWrong);
 }
+
+btnFourOne.addEventListener("click", functionFourRight);
+btnFourTwo.addEventListener("click", functionFourWrong);
+btnFourOne.addEventListener("click", questionMaker);
+btnFourTwo.addEventListener("click", questionMaker);
+
+console.log(questionNumb);
