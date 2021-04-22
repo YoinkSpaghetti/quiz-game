3dbd8a767841af55f79a376a1142720af1be3430
YoinkSpaghetti <michael.mo23@sitechhs.com>
2021-04-22 17:27:32 -0400

    final update

--- src/js/index.js
+++ src/js/index.js
@@ -1,17 +1,20 @@
-var x = document.getElementById("startQuiz");
-var y = document.getElementById("questions");
-var z = document.getElementById("questionsTwo");
-var oneCorrect = document.getElementById("correctOne");
-var oneWrong = document.getElementById("wrongOne");
-var theEnd = document.getElementById("end");
-var btn = document.getElementById("yesBtn");
-var btnTwo = document.getElementById("okayBtn");
-var btnThreeOne = document.getElementById("firstAnswer");
-var btnThreeTwo = document.getElementById("secondAnswer");
-var btnThreeThree = document.getElementById("thirdAnswer");
-var btnThreeFour = document.getElementById("fourthAnswer");
-var btnFourOne = document.getElementById("continueButtonOne");
-var btnFourTwo = document.getElementById("continueButtonTwo");
+const x = document.getElementById("startQuiz");
+const y = document.getElementById("questions");
+const z = document.getElementById("questionsTwo");
+const oneCorrect = document.getElementById("correctOne");
+const oneWrong = document.getElementById("wrongOne");
+const theEnd = document.getElementById("end");
+const arrayOne = [x, y, z];
+counterOne = 0;
+
+const btn = document.getElementById("yesBtn");
+const btnTwo = document.getElementById("okayBtn");
+const btnThreeOne = document.getElementById("firstAnswer");
+const btnThreeTwo = document.getElementById("secondAnswer");
+const btnThreeThree = document.getElementById("thirdAnswer");
+const btnThreeFour = document.getElementById("fourthAnswer");
+const btnFourOne = document.getElementById("continueButtonOne");
+const btnFourTwo = document.getElementById("continueButtonTwo");
 let score = 0;
 /* var timerLength = document.getElementById("timerid");
 var a = timerLength.style.width; */
@@ -23,8 +26,8 @@ const theQuestions = [
   "what is brown and sticky?",
   "what colour underwear am I wearing now?!?1",
 ];
-var rightAnswer = ["4", "Orange", "a stick", "I don't know"];
-var wrongAnswer = [
+const rightAnswer = ["4", "Orange", "a stick", "I don't know"];
+let wrongAnswer = [
   "click me",
   "no",
   "yellow",
@@ -41,14 +44,11 @@ var wrongAnswer = [
 
 //switches page
 function functionOne() {
-  x.style.display = "none";
-  y.style.display = "block";
+  arrayOne[counterOne].style.display = "none";
+  arrayOne[counterOne + 1].style.display = "block";
+  counterOne = counterOne + 1;
 }
 //switches page
-function functionTwo() {
-  y.style.display = "none";
-  z.style.display = "block";
-}
 /* function sleep(milliseconds) {
   const date = Date.now();
   let currentDate = null;
@@ -113,13 +113,13 @@ function questionMaker() {
   //taking a random value from wrong Answer and the question number of right Answer and putting them in an array
   questionNumbar(questionNumb); //this changes the question depending on the number or the other way around i dunno
   document.getElementById("points").innerHTML = "Points: " + score;
-  var n = randomNumber(0, wrongAnswer.length);
-  var nOne = randomNumber(0, wrongAnswer.length - 1);
-  var nTwo = randomNumber(0, wrongAnswer.length - 2);
-  var nThree = randomNumber(0, 4);
-  var nFour = randomNumber(0, 3);
-  var nFive = randomNumber(0, 2);
-  var nSix = randomNumber(0, 1);
+  const n = randomNumber(0, wrongAnswer.length);
+  const nOne = randomNumber(0, wrongAnswer.length - 1);
+  const nTwo = randomNumber(0, wrongAnswer.length - 2);
+  const nThree = randomNumber(0, 4);
+  const nFour = randomNumber(0, 3);
+  const nFive = randomNumber(0, 2);
+  const nSix = randomNumber(0, 1);
   allAnswers = [rightAnswer[questionNumb]];
   function insideQuestionMaker(banana) {
     allAnswers.push(wrongAnswer[banana]);
@@ -129,7 +129,7 @@ function questionMaker() {
   insideQuestionMaker(nOne);
   insideQuestionMaker(nTwo);
   //then putting it a new shuffled array
-  var allAnswersTwo = [];
+  const allAnswersTwo = [];
   function insideQuestionMakerAgain(apple) {
     allAnswersTwo.push(allAnswers[apple]);
     allAnswers.splice(apple, 1);
@@ -189,7 +189,7 @@ function checkCorrectAnswer() {
 }
 
 btn.addEventListener("click", functionOne);
-btnTwo.addEventListener("click", functionTwo);
+btnTwo.addEventListener("click", functionOne);
 
 /* btnTwo.addEventListener("click", timerFunction); */
 /* btnThree.addEventListener("click", functionThree); */
